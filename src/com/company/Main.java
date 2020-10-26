edc2157c8e0c85612988ffa2130fbe594c6a79fe
vladimirtischler <tischler.vladimir@gmail.com>
2020-10-26 18:31:12 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,1,1,2,2,3,4,5);
+        System.out.println(numbers);
+    }
+}
