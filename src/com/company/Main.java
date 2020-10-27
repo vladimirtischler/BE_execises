73a9d1193dac5473239e66bfa665e4770ce4560a
vladimirtischler <tischler.vladimir@gmail.com>
2020-10-27 20:58:25 +0100

    Do BE exercises

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,35 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //1.cvičenie
-        //1.verzia
-        List<Integer> numbers1 = Arrays.asList(1,1,1,2,2,3,4,5);
-        numbers1.stream().forEach(y ->System.out.print(y));
-        System.out.println();
-        //2.verzia
-        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
-        numbers2.add(1);
-        numbers2.add(1);
-        numbers2.add(1);
-        numbers2.add(2);
-        numbers2.add(2);
-        numbers2.add(3);
-        numbers2.add(4);
-        numbers2.add(5);
-        numbers2.forEach( (n) -> { System.out.print(n); } );
-        System.out.println();
+    public static ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-        //2.cvičenie
+    public static void main(String[] args) {
+        //1.exercise
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
         Thread numbers = new Thread(Main::numbers3);
         numbers.start();
 
+        //2.exercise
+        addIfNotExist(5);
     }
-    public static void numbers3(){
-        List<Integer> numbers = Arrays.asList(1,1,1,2,2,3,4,5);
+    public static List<Integer> numbers3(){
         numbers.stream().forEach(y->System.out.print(y));
+        return numbers;
+    }
+
+    public static List<Integer> addIfNotExist(int number) {
+        if (!numbers.contains(number))
+        {
+            numbers.add(number);
+        }
+        return numbers;
     }
 }
