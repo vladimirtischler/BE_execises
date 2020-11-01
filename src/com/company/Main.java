45714d50e3b13da804a4360bfe11e3dae991422a
vladimirtischler <tischler.vladimir@gmail.com>
2020-11-01 19:55:22 +0100

    edit exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,28 +7,30 @@ import java.util.List;
 
 public class Main {
 
-    public static List<Integer> numbers = new ArrayList<Integer>();
+    public static List<Integer> numbers = new ArrayList();
 
     public static void main(String[] args) {
         //1.exercise
         numbers.addAll(Arrays.asList(1,1,1,2,2,3,4,5));
 
-        Thread numbers = new Thread(Main::numbers3);
-        numbers.start();
+        numbers3();
+        System.out.println();
 
         //2.exercise
-        addIfNotExist(10);
+        addIfNotExist(78);
+
+        //3.exercise
+
     }
-    public static List<Integer> numbers3(){
-        numbers.stream().forEach(y->System.out.print(y));
-        return numbers;
+    public static void numbers3(){
+        numbers.stream().forEach(System.out::print);
     }
 
-    public static List<Integer> addIfNotExist(int number) {
+    public static void addIfNotExist(int number) {
         if (!numbers.contains(number))
         {
             numbers.add(number);
+            numbers.stream().forEach(System.out::print);
         }
-        return numbers;
     }
 }
